956bc62b34d3b142dba37f8f724bec2533d22913
Tom <thomas.spencer@codurance.com>
2021-09-14 08:09:18 +0100

    start prime factors

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[allow(dead_code)]
+fn prime_factors(num: i64) -> Vec<i64>{
+    vec![num]
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prime_factors;
+
+    #[test]
+    fn prime_factors_of_two() {
+        assert_eq!(prime_factors(2), [2]);
+    }
+
+    #[test]
+    fn prime_factors_of_three() {
+        assert_eq!(prime_factors(3), [3]);
+    }
+}
+
