b1e0923922438a2f94c4bc48287da75a2fee76b6
Tom <thomas.spencer@codurance.com>
2021-09-14 17:45:20 +0100

    add conditional return

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #[allow(dead_code)]
 fn prime_factors(num: i64) -> Vec<i64>{
-    vec![num]
+    if num % 2 == 0 && num > 2 {
+        [vec![2], prime_factors(num / 2)].concat()
+    } else {
+        vec![num]
+    }
 }
 
 #[cfg(test)]
@@ -16,5 +20,8 @@ mod tests {
     fn prime_factors_of_three() {
         assert_eq!(prime_factors(3), [3]);
     }
+
+    #[test]
+    fn prime_factors_of_four() { assert_eq!(prime_factors(4), [2, 2]); }
 }
 
